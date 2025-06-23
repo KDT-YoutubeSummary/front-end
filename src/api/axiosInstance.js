858018141da24957de60b1bb54c65a55b099e832
정수 <poker97.123.321.111.223@gmail.com>
2025-06-23 14:20:07 +0900

    url 추가 수정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,11 +1,5 @@
-// src/api/axiosInstance.js
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://43.203.114.254:8080/api',
-    auth: {
-        username: 'root',
-        password: '1234'
-    }
+    baseURL: '/api',
 });
-export default api;
