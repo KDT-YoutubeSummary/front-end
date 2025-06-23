11498fc37e32260ae4cdbf6fda5a86b518ef458b
정수 <poker97.123.321.111.223@gmail.com>
2025-06-23 14:31:58 +0900

    url 추가 수정

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://43.203.114.254:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+    port: 5173
+  }
 })
