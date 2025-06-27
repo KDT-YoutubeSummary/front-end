725689bbe2478432dfed1b5418f718a9f09a4ae0
정수 <poker97.123.321.111.223@gmail.com>
2025-06-27 22:45:41 +0900

    api.jsx 수정

--- src/api/reminderApi.js
+++ src/api/reminderApi.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const reminderApi = {
     getReminders: async (summaryId) => {
         try {
--- src/api/summaryArchiveApi.js
+++ src/api/summaryArchiveApi.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const summaryArchiveApi = {
     getAllSummaries: async () => {
         try {
